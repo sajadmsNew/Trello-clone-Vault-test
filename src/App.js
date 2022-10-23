19cc20e1c3112f0537ef9064cfa1584e2e423e88
sajadmsNew <smousavi.ee@gmail.com>
2022-10-23 16:20:00 +0300

    feat/add-structure-ui-context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import UIProvider from "./context/UIContext";
+import Routes from "./routes/Routes";
 
-function App() {
+const App = () => {
+  const [openBackdrop, setOpenBackdrop] = useState(false);
+  const [renderedBoard, setRenderedBoard] = useState();
+
   return (
+    <UIProvider
+      openBackdrop={openBackdrop}
+      setOpenBackdrop={setOpenBackdrop}
+      renderedBoard={renderedBoard}
+      setRenderedBoard={setRenderedBoard}
+    >
+          <div className="App">
+            <Routes />
+          </div>
+       
     
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </UIProvider>
   );
-}
+};
 
 export default App;
